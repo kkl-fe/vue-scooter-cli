0fdc7ef3e4f46ba4c86fed893eb967d9ca938487
glf <bluekylin@gmail.com>
2020-12-22 23:23:11 +0800

    build test

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log(123)
+console.log(process.npm_config_build, 111);
+const path = require('path');
+const buildTool = require('../build-tool');
+// 读取项目根目录vue-scooter.config.js配置
+let workspace = process.cwd();
+let customConfig = require(path.resolve(workspace, './vue-scooter.config.js'));
+let buildConfig = customConfig.build;
+let buildSrc = path.resolve(workspace, buildConfig.src);
+let buildTarget = path.resolve(workspace, buildConfig.dist);
+buildTool.run(buildSrc, buildTarget);
