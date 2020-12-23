99bbfad332efcc6b4afcf15ce2ef1ac43c0a90b8
glf <bluekylin@gmail.com>
2020-12-24 00:09:37 +0800

    feat: config change

--- bin/index.js
+++ bin/index.js
@@ -4,14 +4,18 @@ const buildTool = require('../build-tool');
 const getopts = require('getopts');
 let opts = getopts(process.argv);
 // 读取项目根目录vue-scooter.config.js配置
-let workspace = process.cwd();
-let customConfig = require(path.resolve(workspace, './vue-scooter.config.js'));
+let projectRoot = process.cwd();
+let customConfig = require(path.resolve(
+  projectRoot,
+  './vue-scooter.config.js'
+));
+
 if (opts.build) {
   let buildConfig = customConfig.build;
-  let buildSrc = path.resolve(workspace, buildConfig.src);
-  let buildTarget = path.resolve(workspace, buildConfig.dist);
+  let buildSrc = path.resolve(projectRoot, customConfig.workspace || './src');
+  let buildTarget = path.resolve(projectRoot, buildConfig.dist);
   buildTool.run(buildSrc, buildTarget);
 }
-if(opts.dev){
-    console.log('dev')
+if (opts.dev) {
+  console.log('dev');
 }
