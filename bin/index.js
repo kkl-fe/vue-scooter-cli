764bb335370295ac2b65c93fc4552b5280a5197f
glf <bluekylin@gmail.com>
2020-12-24 00:00:56 +0800

    feat: support build opts

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-console.log(process.npm_config_build, 111);
 const path = require('path');
 const buildTool = require('../build-tool');
+const getopts = require('getopts');
+let opts = getopts(process.argv);
 // 读取项目根目录vue-scooter.config.js配置
 let workspace = process.cwd();
 let customConfig = require(path.resolve(workspace, './vue-scooter.config.js'));
-let buildConfig = customConfig.build;
-let buildSrc = path.resolve(workspace, buildConfig.src);
-let buildTarget = path.resolve(workspace, buildConfig.dist);
-buildTool.run(buildSrc, buildTarget);
+if (opts.build) {
+  let buildConfig = customConfig.build;
+  let buildSrc = path.resolve(workspace, buildConfig.src);
+  let buildTarget = path.resolve(workspace, buildConfig.dist);
+  buildTool.run(buildSrc, buildTarget);
+}
+if(opts.dev){
+    console.log('dev')
+}
